49069f99956040d651c1cc43ac9def962fef05bc
Breno Henrique <brenosc2@hotmail.com>
2017-02-02 15:01:43 -0200

    adding footer.js

---
+++ client/footer/footer.js
@@ -0,0 +1,12 @@
+Template.footer.helpers({
+  // foo() {
+  //   return Template.instance().counter.get();
+  // },
+});
+
+Template.footer.events({
+  // 'click button'(event, instance) {
+  //   // increment the counter when button is clicked
+  //   instance.counter.set(instance.counter.get() + 1);
+  // },
+});
