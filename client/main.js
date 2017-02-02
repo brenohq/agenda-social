d78e202eee359334b9c2aabbe7f2c3f70a028d9a
Breno Henrique <brenosc2@hotmail.com>
2017-02-02 14:21:35 -0200

    adding home and footer templates

--- client/main.js
+++ client/main.js
@@ -1,20 +1,10 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
+Template.footer.helpers({
+  foo() {
     return Template.instance().counter.get();
   },
 });
 
-Template.hello.events({
+Template.footer.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
