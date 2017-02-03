c08ba0b648ee6e112ba2c9c034faf2ec2c90215f
Andre Kenji Yai <yai.andre@gmail.com>
2017-02-03 12:57:15 -0200

    adding future events to array

--- server/methods.js
+++ server/methods.js
@@ -4,8 +4,52 @@
 
 Meteor.methods({
    'getEventsFromApi': function(){
-      console.log("Meteor",Meteor.user());
+      let accessToken = Meteor.user().services.facebook.accessToken;
+      let page_id = Meteor.user().profile.selectedPage.id;
+      let userId = Meteor.userId();
+      
+      console.log("Meteor",Meteor.user(),"AccessToken:",accessToken,"Page_id:",page_id);
+      let pageUrl = 'https://graph.facebook.com/v2.8/'+ page_id +'?access_token='+ accessToken;
+      pageUrl += '?&fields=events';
+      let eventsResult = Meteor.http.get(pageUrl);
+      let futureEvents = [];
+      if(eventsResult.data.events.data){
+         futureEvents = getFutureEvents(eventsResult.data.events.data);
+      }
+      futureEvents.forEach(function(event){
+         pageUrl = 'https://graph.facebook.com/v2.8/'+ event.id +'?access_token='+ accessToken;
+         pageUrl += '?&fields=cover';
+         let eventsResult = Meteor.http.get(pageUrl);
+         console.log("eventsResult:",eventsResult);
 
+         if(eventsResult.data.cover.source){
+            event.coverImage = eventsResult.data.cover.source;
+         }
+
+      });
+      console.log("userId:",userId,futureEvents);
+
+      Meteor.users.update(userId, { $set: { 'profile.events': futureEvents} }, () => {
+         // CB
+        console.log("Profile",Meteor.user().profile);
+      });
    }
 
 });
+
+
+var getFutureEvents = function(events){
+
+   let futureEvents = [];
+
+
+   events.forEach(function(event){
+
+      if(new Date(event.start_time) > new Date()){
+         futureEvents.push(event);
+      }
+
+   });
+
+   return futureEvents;
+}
