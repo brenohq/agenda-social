88fc6d8a8b4ed10842a7104d238fd55874d08369
Breno Henrique <brenosc2@hotmail.com>
2017-02-03 13:45:44 -0200

    adding event list template

--- client/dashboard/dashboard.js
+++ client/dashboard/dashboard.js
@@ -3,62 +3,64 @@ let time = 0;
 Session.setDefault('progress', 0);
 
 Template.dashboard.helpers({
-	userPages() {
-		if (Meteor.user()) {
-			return Meteor.user().profile.pages;
-		}
+  userPages() {
+    if (Meteor.user()) {
+      return Meteor.user().profile.pages;
+    }
 
-		return []
-	},
+    return []
+  },
 
-	selectedPage() {
-		if (Meteor.user() &&  Meteor.user().profile.selectedPage !== null) {
-			return Meteor.user().profile.selectedPage;
-		}
-	},
+  selectedPage() {
+    if (Meteor.user() && Meteor.user().profile.selectedPage !== null) {
+      return Meteor.user().profile.selectedPage;
+    }
+  },
 
-	getEvents() {
-		return false;
-	}
 });
 
 Template.pageEvents.helpers({
-	getProgress() {
-		// Mockando progresso
-		clearTimeout(timer);
-		timer = setTimeout(() => {
-			let progress = Session.get('progress');
-			if (progress > 100) {
-				clearTimeout(timer);
-				console.log("Finished;");
+  getEvents() {
+    return Meteor.user().profile.events;
+  },
 
-				Meteor.call('getEventsFromApi',Meteor.user(),function(){
+  getProgress() {
+    // Mockando progresso
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      let progress = Session.get('progress');
+      if (progress > 100) {
+        clearTimeout(timer);
+        $('.progress').remove();
+        Meteor.call('getEventsFromApi', Meteor.user(), function() {
 
-				});
+        });
 
-				return;
-			}
+        return;
+      }
 
-			Session.set('progress', progress + 2)
-		}, 100);
+      Session.set('progress', progress + 2)
+    }, 100);
 
-		return Session.get('progress');
+    return Session.get('progress');
 
-	}
+  }
 });
 
-
-
 Template.dashboard.events({
-	'click .card': (e, tpl) => {
-		let page = Blaze.getData(e.target);
-		let userId = Meteor.userId();
+  'click .card': (e, tpl) => {
+    let page = Blaze.getData(e.target);
+    let userId = Meteor.userId();
 
-		Session.set("selectedPage",page);
-		Session.set('progress',0);
-		Meteor.users.update(userId, { $set: { 'profile.selectedPage': page } }, () => {
-			// CB
-		});
-	}
+    Session.set("selectedPage", page);
+    Session.set('progress', 0);
+    Meteor.users.update(userId, {
+      $set: {
+        'profile.selectedPage': page
+      }
+    }, () => {
+      // CB
+    });
+  }
 
 });
